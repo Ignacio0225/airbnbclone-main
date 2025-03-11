c22a3d8af1ddd2ddb7632fac5518fd11fc3abbce
유선혁 <tjsgur2399@icloud.com>
2025-03-11 22:55:49 +0900

    03112255

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("amenities/",views.Amenities.as_view()),
+    path("ameities/<int:pk>",views.AmenityDetail.as_view())
 ]
